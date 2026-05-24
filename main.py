8161c379b89c543f45ca4239d37bed67949f3e55
ramielchik01 <ramiel_0101@icloud.com>
2026-05-24 06:40:18 +0000

    May 23, 2026, 11:40 PM

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/users/{user_id}')
 def get_user(user_id: int):
     return {user_id }
+
+#Query Parametr 
+@app.get('/users1Query')
+def get_userQuery(limit: int=10, skip: int=0):
+    return {'limit': limit, 'skip': skip}
